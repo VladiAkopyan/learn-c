a04afca80fa80f5504822fd0b24fb5d3fc362f09
Vladi Akopyan <vakopan686@gmail.com>
2026-09-03 00:42:43 +0300

    first commit

---
+++ Calculate/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    char operation;
+    float result;
+
+    while (1) {
+        printf(" >> ");
+        scanf("%d %c %d", &num1, &operation, &num2);
+        printf("\n");
+
+        
+        switch(operation) {
+            case '+':
+                result = num1 + num2;
+                printf("%d %c %d = %.2f\n", num1, operation, num2, result);
+                break;
+            case '-':
+                result = num1 - num2;
+                printf("%d %c %d = %.2f\n", num1, operation, num2, result);
+                break;
+            case '*':
+                result = num1 * num2;
+                printf("%d %c %d = %.2f\n", num1, operation, num2, result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = (float)num1 / num2;
+                    printf("%d %c %d = %.2f\n", num1, operation, num2, result);
+                    break;
+                }
+                else {
+                    printf("Zero Error\n");
+            }
+        }
+    }
+}
